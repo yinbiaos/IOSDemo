8e3a3f67b6cd74fef168a554d95bb55083ed5f25
强智科技 <qzkj@qzkj-macmini.local>
2019-09-23 08:42:22 +0800

    init demo

---
+++ firstios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstios
+//
+//  Created by 强智科技 on 2019/9/20.
+//  Copyright © 2019年 强智科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstios/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  firstios
+//
+//  Created by 强智科技 on 2019/9/20.
+//  Copyright © 2019年 强智科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
